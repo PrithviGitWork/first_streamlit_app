1bbd949070ce8586b6cc47994badca5a24f052be
Prithvi Venkatesh <117724359+PrithviGitWork@users.noreply.github.com>
2023-02-10 19:28:59 +0530

    Update streamlit_app.py
    Adding proper BUTTON header

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ try:
 except URLError as e:
   streamlit.error()
 
-streamlit.header("The FRUIT LOAD LIST TABLE contains:")
+streamlit.header("View our Fruit List - Add your Favorites!")
 #snowflake related functions
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
@@ -51,7 +51,7 @@ def get_fruit_load_list():
     return my_cur.fetchall()
  
 #Add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_row = get_fruit_load_list()
   my_cnx.close()
